3652019dd95944f64452d9fd2832c47ea49ff5f3
kiyong <dky0979@gmail.com>
2023-11-27 15:14:41 +0900

    no message

--- Package.swift
+++ Package.swift
@@ -16,11 +16,11 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "FoundationResources",
-            dependencies: [],
-            resources: [.process("Resources/jsonFile.json")]),
+            dependencies: []),
         .target(
             name: "MetFoundation",
-            dependencies: ["FoundationResources"]),
+            dependencies: [],
+            resources: [.process("Resources/jsonFile.json")]),
         .testTarget(
             name: "MetFoundationTests",
             dependencies: ["MetFoundation"]),
--- Sources/FoundationResources/SharedResource.swift
+++ Sources/FoundationResources/SharedResource.swift
@@ -5,9 +5,9 @@
 //  Created by Do Kiyong on 11/27/23.
 //
 
-import Foundation
-
-public enum SharedResource {
-    static public let bundle: Bundle = Bundle.module
-    static public let jsonPath: String? = Bundle.module.path(forResource: "jsonFile", ofType: "json")
-}
+//import Foundation
+//
+//public enum SharedResource {
+//    static public let bundle: Bundle = Bundle.module
+//    static public let jsonPath: String? = Bundle.module.path(forResource: "jsonFile", ofType: "json")
+//}
